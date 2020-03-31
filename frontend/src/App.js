a120805ed42a692f0e9a69dab2cff42b84b48c9b
AlephTav Corp <alephtavcorp@AlephTavCorp-iMac.local>
2020-03-31 12:35:15 -0300

    front-end web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import Routers from './routes';
 
-function App() {
+
+export default function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello AlephTav.
-        </p>
-        <a
-          className="App-link"
-          href="https://alephtavcorp.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          A L E P H T A V  C O R P
-        </a>
-      </header>
-    </div>
+    <Routers />
   );
 }
-
-export default App;
